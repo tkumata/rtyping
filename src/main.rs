cc8d827511e129d2ec0e1ec1d9f7ca85fe6ae945
tkumata <tkumata@gmail.com>
2023-06-10 13:41:19 +0900

    feat: No.1 termion で色付ける

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,64 @@
 //
 // Typing game
 //
+extern crate termion;
+
 use std::io;
 use std::fs;
 use rand::Rng;
 use std::thread;
 use std::time::Duration;
 use std::process;
+use termion::{color, style};
 
 fn main() {
+    let pickup_words : usize = 3;
+
     println!("==> Typing game");
 
+    // init vector which save words
     let mut words = Vec::new();
 
+    // push words as string to the vector
     for entry in fs::read_dir("/usr/bin").unwrap() {
         words.push(
             String::from(entry.unwrap().path().file_name().unwrap().to_str().unwrap())
         );
     }
 
+    // vector length
     let len = words.len();
 
     loop {
         // count 30 sec on background
         let _handle = thread::spawn(|| {
             for _sec in 1..30 {
+                // todo: through the variable from thread to the any function
+
+                // thread.sleep
                 thread::sleep(Duration::from_millis(1000));
             }
 
-            println!("==> Time up (30 sec)");
+            println!(
+                "==> {color_red}Time up{reset} (30 sec)",
+                color_red = color::Fg(color::Red),
+                reset = style::Reset
+            );
             println!("==> Quit process");
 
             process::exit(0);
         });
 
         let mut rnd = rand::thread_rng();
-        let i = rnd.gen_range(0..len - 5);
-        let j = i + 5;
+        let i = rnd.gen_range(0..len - pickup_words);
+        let j = i + pickup_words;
         let sample_string:String = words[i..=j].join(" ");
 
-        println!("==> Type following words.");
+        println!(
+            "==> {color_red}Type following words.{reset}",
+            color_red = color::Fg(color::Red),
+            reset = style::Reset
+        );
         println!("{}", sample_string);
 
         let mut input = String::new();
@@ -49,9 +68,18 @@ fn main() {
 
         // check string
         if input.trim() == sample_string.trim() {
-            println!("==> OK");
+            println!(
+                "==> 🟢{color_green}OK{reset}💮",
+                color_green = color::Fg(color::Green),
+                reset = style::Reset
+            );
+            println!("==> Try next words");
         } else {
-            println!("==> NG");
+            println!(
+                "==> ❌{color_red}NG{reset}",
+                color_red = color::Fg(color::Red),
+                reset = style::Reset
+            );
             println!("==> Quit process");
             process::exit(0);
         }
