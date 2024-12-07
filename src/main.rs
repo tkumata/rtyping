b3ca37b2e1ba144211b91ee3d7247143ed7d535f
Tomokatsu Kumata <tkumata@gmail.com>
2024-12-07 15:06:00 +0000

    fix WPM

--- src/main.rs
+++ src/main.rs
@@ -213,5 +213,5 @@ fn load_words(level: usize) -> String {
 }
 
 fn calc_wpm(inputs_length: usize, seconds: i32, incorrect: i32) -> f64 {
-    (inputs_length as f64 - incorrect as f64) / (5.0 * (seconds as f64 / 60.0))
+    (inputs_length as f64 - incorrect as f64) / (5.0 * seconds as f64 / 60.0)
 }
