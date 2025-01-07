00e2c8cdc47ef51bf6b8a6049c9745fc0a2acff2
Tomokatsu Kumata <tkumata@gmail.com>
2025-01-07 11:03:10 +0000

    fix: fix bug

--- src/domain/entity.rs
+++ src/domain/entity.rs
@@ -1,14 +1,6 @@
-use std::fs;
 use std::io;
 
 pub fn get_sample() -> Result<String, io::Error> {
-    let file_path = "src/domain/sample.txt";
-
-    match fs::read_to_string(file_path) {
-        Ok(contents) => Ok(contents),
-        Err(err) => {
-            eprintln!("Error reading file: {}", err);
-            Err(err)
-        }
-    }
+    let contents = include_str!("sample.txt").to_string();
+    Ok(contents)
 }
